06431b445e2b09ec2ed61a03d2fbf4781bcc39d9
ahmadraza348 <engr.ahmadraza348@gmail.com>
2025-08-11 12:06:39 +0500

    Header working

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <header className="shadow ">
+        <div className="bg-dark text-center py-3">
+          <span className="text-white">Your Fashion Partner</span>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Home;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/style.scss"
 import App from './App.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
